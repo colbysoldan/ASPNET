fbc556f31972d8394abd5dbdf9021b66bccbaa8a
Colby Soldan <colbylsoldan@gmail.com>
2022-07-07 09:55:30 -0500

    finished part 1

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository()
+        {
+        }
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
+
